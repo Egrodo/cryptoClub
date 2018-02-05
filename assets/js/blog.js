c1c4820b1f416cad703073c7efaad11f617c144e
Egrodo <egrodo1@gmail.com>
2018-02-04 20:34:05 -0500

    Created a blog page the underlying code for it. Also put ticker code in its own file so no crossover.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,25 +1,34 @@
+// Custom Medium posts Ret	riever by Noah Yamamoto
+
+// Uses rss2json to request rss feed from Medium and display it properly.
 const retrieve = () => {
-	fetch('https://api.rss2json.com/v1/api.json?api_key=4iwmzf0yryfd6ao88oqtqao8cxrwxrdefcutllj4&rss_url=https://medium.com/feed/@blockchainbaruch&count=1')
+	fetch('https://api.rss2json.com/v1/api.json?api_key=4iwmzf0yryfd6ao88oqtqao8cxrwxrdefcutllj4&rss_url=https://medium.com/feed/@blockchainbaruch&count=3')
 	.then(res => {
 		if (res.ok) {
 			return res.json();
 		} else throw new Error('Connection to rss api failed.');
-	}).then(data => {
-		console.log(data);
-		display(data);
+	}).then(posts => {
+		blogDisp(posts);
 	}).catch(err => {
 		console.error('There was a problem with the fetch:' + err.message);
 	});
 };
 
-const display = data => {
-	let title = document.createElement('h1');
-		title.innerHTML = data.items[0].title;
-	let content = document.createElement('span');
-		content.innerHTML = data.items[0].content;
-		console.log(content);
-	document.getElementById('title').appendChild(title);
-	document.getElementById('display').appendChild(content);
+const blogDisp = posts => {
+	// For each postss, display it.
+	for (let i = 0; i < posts.items.length; i++) {
+		let display = document.createElement('div');
+		let title = document.createElement('h3');
+		let date = document.createElement('h5');
+		let content = document.createElement('div');
+		title.innerHTML = posts.items[i].title;
+		date.innerHTML = `<p>By: <a href="https://medium.com/@Blockchainbaruch">${posts.items[i].author}</a> <br> ${posts.items[i].pubDate.substr(0,posts.items[i].pubDate.indexOf(' '))}</p>`;
+		content.innerHTML = posts.items[i].content;
+		display.appendChild(title);
+		display.appendChild(date);
+		display.appendChild(content);
+		document.getElementById('blog').appendChild(display);
+	}
 };
 
 retrieve();
