ca4f970e87641263da5bfe2be11a5fe139a06c8c
Egrodo <egrodo1@gmail.com>
2018-02-14 11:23:24 -0500

    Updated blog URL

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,7 +2,7 @@
 
 // Uses rss2json to request rss feed from Medium and display it properly.
 const retrieve = () => {
-	fetch('https://api.rss2json.com/v1/api.json?api_key=4iwmzf0yryfd6ao88oqtqao8cxrwxrdefcutllj4&rss_url=https://medium.com/@cunycrypto&count=3')
+	fetch('https://api.rss2json.com/v1/api.json?api_key=4iwmzf0yryfd6ao88oqtqao8cxrwxrdefcutllj4&rss_url=https://medium.com/feed/@cunycrypto')
 	.then(res => {
 		if (res.ok) {
 			return res.json();
@@ -16,13 +16,14 @@ const retrieve = () => {
 
 const blogDisp = posts => {
 	// For each postss, display it.
+	console.log(posts);
 	for (let i = 0; i < posts.items.length; i++) {
 		let display = document.createElement('div');
 		let title = document.createElement('h3');
 		let date = document.createElement('h5');
 		let content = document.createElement('div');
 		title.innerHTML = posts.items[i].title;
-		date.innerHTML = `<p>By: <a href="https://medium.com/@Blockchainbaruch">${posts.items[i].author}</a> <br> ${posts.items[i].pubDate.substr(0,posts.items[i].pubDate.indexOf(' '))}</p>`;
+		date.innerHTML = `<p>By: <a href="https://medium.com/@cunycrypto">${posts.items[i].author}</a> <br> ${posts.items[i].pubDate.substr(0,posts.items[i].pubDate.indexOf(' '))}</p>`;
 		content.innerHTML = posts.items[i].content;
 		display.append(title, date, content);
 		document.getElementById('blog').appendChild(display);
